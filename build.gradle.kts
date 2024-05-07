e7e9c897882ef4aa2f78c9b0920a48c3940bcaf6
balasubramanian-rengasamy <vrbala@gmail.com>
2024-05-07 04:37:22 -0400

    simple shell script

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,39 +32,3 @@ allOpen {
   )
 }
 
-
-// Add these lines at the top of your build.gradle.kts file if needed
-extra["payaraHome"] = "/Users/bala/_app_servers/payara6"
-extra["contextRoot"] = "demo-pms-app"
-
-// Helper function to create the common Exec configuration
-private fun configureAsadminExec(task: Exec) {
-  task.workingDir = extra["payaraHome"] as File
-  val asadminFile = if (System.getProperty("os.name").lowercase(Locale.getDefault()).contains("windows")) {
-    // Windows
-    listOf(File(task.workingDir, "bin${File.separator}asadmin.bat").toString())
-  } else {
-    // Unix-like OS (including macOS)
-    listOf(File(task.workingDir, "bin${File.separator}asadmin").toString())
-  }
-  task.commandLine = asadminFile
-}
-
-tasks {
-  register<Exec>("startServer") {
-    configureAsadminExec(this)
-    args("start-domain")
-  }
-
-  register<Exec>("stopServer") {
-    configureAsadminExec(this)
-    args("stop-domain")
-  }
-
-  register<Exec>("redeploy") {
-    dependsOn("war")
-    configureAsadminExec(this)
-    val warAppName = war.get().outputs.files.singleFile.name.substringBeforeLast('.')
-    val warPath = war.get().archiveFile.get().asFile.absolutePath
-    args("deploy", "--force=true", extra["contextRoot"], "--name=$warAppName", warPath)}
-}
---
+++ payara.sh
@@ -0,0 +1,1 @@
+./gradlew build && cp build/libs/demo-pms-app-1.0-SNAPSHOT.war ~/_app_servers/payara6/glassfish/domains/domain1/autodeploy
